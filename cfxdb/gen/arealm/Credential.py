992e6fd7a2a2e10839f1330da060b8e55ef78c5c
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-08-14 22:46:44 +0200

    sync

--- cfxdb/gen/arealm/Credential.py
+++ cfxdb/gen/arealm/Credential.py
@@ -21,62 +21,10 @@ class Credential(object):
     def Init(self, buf, pos):
         self._tab = flatbuffers.table.Table(buf, pos)
 
-    # WAMP authentication method offered by the authenticating client.
+    # ID of this credential.
     # Credential
-    def Authmethod(self):
+    def Oid(self):
         o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(4))
-        if o != 0:
-            return self._tab.Get(flatbuffers.number_types.Int8Flags, o + self._tab.Pos)
-        return 0
-
-    # WAMP realm requested by the authenticating client.
-    # Credential
-    def Realm(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
-        if o != 0:
-            return self._tab.String(o + self._tab.Pos)
-        return None
-
-    # WAMP authid announced by the authenticating client.
-    # Credential
-    def Authid(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
-        if o != 0:
-            return self._tab.String(o + self._tab.Pos)
-        return None
-
-    # Authentication method specific configuration (CBOR serialized).
-    # Credential
-    def Authconfig(self, j):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
-        if o != 0:
-            a = self._tab.Vector(o)
-            return self._tab.Get(flatbuffers.number_types.Uint8Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 1))
-        return 0
-
-    # Credential
-    def AuthconfigAsNumpy(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
-        if o != 0:
-            return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Uint8Flags, o)
-        return 0
-
-    # Credential
-    def AuthconfigLength(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
-        if o != 0:
-            return self._tab.VectorLen(o)
-        return 0
-
-    # Credential
-    def AuthconfigIsNone(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
-        return o == 0
-
-    # ID of the principal this credential resolves to upon successful authentication.
-    # Credential
-    def PrincipalOid(self):
-        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
         if o != 0:
             x = o + self._tab.Pos
             from ..oid_t import oid_t
@@ -85,11 +33,76 @@ class Credential(object):
             return obj
         return None
 
-def CredentialStart(builder): builder.StartObject(5)
-def CredentialAddAuthmethod(builder, authmethod): builder.PrependInt8Slot(0, authmethod, 0)
-def CredentialAddRealm(builder, realm): builder.PrependUOffsetTRelativeSlot(1, flatbuffers.number_types.UOffsetTFlags.py_type(realm), 0)
-def CredentialAddAuthid(builder, authid): builder.PrependUOffsetTRelativeSlot(2, flatbuffers.number_types.UOffsetTFlags.py_type(authid), 0)
-def CredentialAddAuthconfig(builder, authconfig): builder.PrependUOffsetTRelativeSlot(3, flatbuffers.number_types.UOffsetTFlags.py_type(authconfig), 0)
+    # WAMP authentication method offered by the authenticating client.
+    # Credential
+    def Authmethod(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(6))
+        if o != 0:
+            return self._tab.Get(flatbuffers.number_types.Int8Flags, o + self._tab.Pos)
+        return 0
+
+    # WAMP realm requested by the authenticating client.
+    # Credential
+    def Realm(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(8))
+        if o != 0:
+            return self._tab.String(o + self._tab.Pos)
+        return None
+
+    # WAMP authid announced by the authenticating client.
+    # Credential
+    def Authid(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(10))
+        if o != 0:
+            return self._tab.String(o + self._tab.Pos)
+        return None
+
+    # Authentication method specific configuration (CBOR serialized).
+    # Credential
+    def Authconfig(self, j):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        if o != 0:
+            a = self._tab.Vector(o)
+            return self._tab.Get(flatbuffers.number_types.Uint8Flags, a + flatbuffers.number_types.UOffsetTFlags.py_type(j * 1))
+        return 0
+
+    # Credential
+    def AuthconfigAsNumpy(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        if o != 0:
+            return self._tab.GetVectorAsNumpy(flatbuffers.number_types.Uint8Flags, o)
+        return 0
+
+    # Credential
+    def AuthconfigLength(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        if o != 0:
+            return self._tab.VectorLen(o)
+        return 0
+
+    # Credential
+    def AuthconfigIsNone(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(12))
+        return o == 0
+
+    # ID of the principal this credential resolves to upon successful authentication.
+    # Credential
+    def PrincipalOid(self):
+        o = flatbuffers.number_types.UOffsetTFlags.py_type(self._tab.Offset(14))
+        if o != 0:
+            x = o + self._tab.Pos
+            from ..oid_t import oid_t
+            obj = oid_t()
+            obj.Init(self._tab.Bytes, x)
+            return obj
+        return None
+
+def CredentialStart(builder): builder.StartObject(6)
+def CredentialAddOid(builder, oid): builder.PrependStructSlot(0, flatbuffers.number_types.UOffsetTFlags.py_type(oid), 0)
+def CredentialAddAuthmethod(builder, authmethod): builder.PrependInt8Slot(1, authmethod, 0)
+def CredentialAddRealm(builder, realm): builder.PrependUOffsetTRelativeSlot(2, flatbuffers.number_types.UOffsetTFlags.py_type(realm), 0)
+def CredentialAddAuthid(builder, authid): builder.PrependUOffsetTRelativeSlot(3, flatbuffers.number_types.UOffsetTFlags.py_type(authid), 0)
+def CredentialAddAuthconfig(builder, authconfig): builder.PrependUOffsetTRelativeSlot(4, flatbuffers.number_types.UOffsetTFlags.py_type(authconfig), 0)
 def CredentialStartAuthconfigVector(builder, numElems): return builder.StartVector(1, numElems, 1)
-def CredentialAddPrincipalOid(builder, principalOid): builder.PrependStructSlot(4, flatbuffers.number_types.UOffsetTFlags.py_type(principalOid), 0)
+def CredentialAddPrincipalOid(builder, principalOid): builder.PrependStructSlot(5, flatbuffers.number_types.UOffsetTFlags.py_type(principalOid), 0)
 def CredentialEnd(builder): return builder.EndObject()
