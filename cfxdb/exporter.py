18a555f4be8923544271dfd9f504f01b7bf49915
Tobias Oberstein <tobias.oberstein@crossbario.com>
2020-09-14 01:39:11 +0200

    sync

--- cfxdb/exporter.py
+++ cfxdb/exporter.py
@@ -9,18 +9,10 @@ import os
 
 import zlmdb
 import cfxdb
-# import cbor2
+import cbor2
 import click
 
 from pprint import pprint
-
-# cfxdb.schema.Schema
-# cfxdb.meta.Schema
-# cfxdb.globalschema.GlobalSchema
-# cfxdb.mrealmschema.MrealmSchema
-# cfxdb.xbr.Schema
-# cfxdb.xbrmm.Schema
-# cfxdb.xbrnetwork.Schema
 
 import uuid
 import binascii
@@ -31,18 +23,39 @@ import numpy as np
 
 
 class Exporter(object):
+    """
+    CFXDB database exporter.
+    """
     def __init__(self, dbpath):
-        self._dbpath = os.path.abspath('.xbrnetwork')
+        """
+
+        :param dbpath: Database file to open.
+        """
+        self._dbpath = os.path.abspath(dbpath)
         self._db = zlmdb.Database(dbpath=self._dbpath, maxsize=2**30, readonly=False)
         self._db.__enter__()
 
+        # cfxdb.schema.Schema
+        # cfxdb.meta.Schema
+        # cfxdb.globalschema.GlobalSchema
+        # cfxdb.mrealmschema.MrealmSchema
+        # cfxdb.xbr.Schema
+        # cfxdb.xbrmm.Schema
+        # cfxdb.xbrnetwork.Schema
+
         self._meta = cfxdb.meta.Schema.attach(self._db)
+        self._globalschema = cfxdb.globalschema.GlobalSchema.attach(self._db)
+        self._mrealmschema = cfxdb.mrealmschema.MrealmSchema.attach(self._db)
         self._xbr = cfxdb.xbr.Schema.attach(self._db)
+        self._xbrmm = cfxdb.xbrmm.Schema.attach(self._db)
         self._xbrnetwork = cfxdb.xbrnetwork.Schema.attach(self._db)
 
         self._schemata = {
-            'meta': self._meta,
+            # 'meta': self._meta,
+            'globalschema': self._globalschema,
+            # 'mrealmschema': self._mrealmschema,
             'xbr': self._xbr,
+            'xbrmm': self._xbrmm,
             'xbrnetwork': self._xbrnetwork,
         }
 
@@ -93,6 +106,7 @@ class Exporter(object):
         print('')
 
     def print_stats(self):
+        print('\nDatabase table statistics [dbpath="{dbpath}"]:\n'.format(dbpath=self._dbpath))
         stats = {}
         with self._db.begin() as txn:
             for schema_name in self._schemata:
@@ -101,33 +115,40 @@ class Exporter(object):
                     table = self._schemata[schema_name].__dict__[table_name]
                     cnt = table.count(txn)
                     stats[schema_name][table_name] = cnt
-        pprint(stats)
+        for schema_name in stats:
+            for table_name in stats[schema_name]:
+                print('{:.<52}: {}'.format(
+                    click.style('{}.{}'.format(schema_name, table_name), fg='white', bold=True),
+                    click.style(str(stats[schema_name][table_name]) + ' records', fg='yellow')))
 
-    # def export(self, filename):
-    #     result = {}
-    #     with self._db.begin() as txn:
-    #         print('YYY', self._xbrnetwork.EXPORTED)
-    #         # for table in xbrnetwork.EXPORTED:
-    #         for tablename in tables:
-    #             if tablename in self._xbrnetwork.__dict__ and not tablename.startswith('idx'):
-    #                 table = self._xbrnetwork.__dict__[tablename]
-    #                 print('XXXXXXXXX', table)
-    #                 recs = []
-    #                 for key, val in table.select(txn, return_keys=True, return_values=True):
-    #                     recs.append((table._serialize_key(key), val.marshal() if val else None))
-    #                 print(table)
-    #                 print(self._xbrnetwork.accounts)
-    #                 result[tablename] = recs
-    #
-    #     from pprint import pprint
-    #     pprint(result)
-    #
-    #     print(cbor2.dumps(result))
-    #
-    #     with open(filename, 'wb') as f:
-    #         f.write(cbor2.dumps(result))
+    def export(self, filename):
+        result = {}
+        with self._db.begin() as txn:
+            for schema_name in self._schemata:
+                for table_name in self._schema_tables[schema_name]:
+                    table = self._schemata[schema_name].__dict__[table_name]
+                    recs = []
+                    for key, val in table.select(txn, return_keys=True, return_values=True):
+                        if val:
+                            if hasattr(val, 'marshal'):
+                                val = val.marshal()
+                        recs.append((table._serialize_key(key), val))
+                    if recs:
+                        if schema_name not in result:
+                            result[schema_name] = {}
+                        result[schema_name][table_name] = recs
+
+        data = cbor2.dumps(result)
+        with open(filename, 'wb') as f:
+            f.write(data)
+
+        data_recovered = cbor2.loads(data)
+        pprint(data_recovered)
 
 
-exporter = Exporter('.xbrnetwork')
-exporter.print_slots()
-exporter.print_stats()
+if __name__ == '__main__':
+    exporter = Exporter('.xbrnetwork')
+    exporter.add_test_data()
+    exporter.print_slots()
+    exporter.print_stats()
+    exporter.export('test.dat')
