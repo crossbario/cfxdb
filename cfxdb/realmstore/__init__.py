64316ff3e4a6f21b2f78d89967862169aead2a3f
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-04-30 17:13:46 +0200

    add table for sessions in realmstore

---
+++ cfxdb/realmstore/__init__.py
@@ -0,0 +1,12 @@
+##############################################################################
+#
+#                        Crossbar.io Database
+#     Copyright (c) Crossbar.io Technologies GmbH. Licensed under MIT.
+#
+##############################################################################
+
+from cfxdb.realmstore.appsession import AppSession
+
+__all__ = (
+    'AppSession',
+)
