ec6c49e2fe529e9e1376a343dcbb2ceb6aa25083
Tobias Oberstein <tobias.oberstein@crossbario.com>
2022-05-01 18:02:02 +0200

    sync

--- cfxdb/__init__.py
+++ cfxdb/__init__.py
@@ -35,13 +35,12 @@ from .common import uint256, unpack_uint256, pack_uint256  # noqa
 from .common import uint128, unpack_uint128, pack_uint128  # noqa
 from .common import uint64, unpack_uint64, pack_uint64  # noqa
 from . import meta, mrealm, xbr, xbrmm, xbrnetwork  # noqa
-from . import globalschema, mrealmschema, eventstore, cookiestore, realmstore  # noqa
+from . import globalschema, mrealmschema, cookiestore, realmstore  # noqa
 
 __all__ = (
     '__version__',
     'meta',
     'mrealm',
-    'eventstore',
     'cookiestore',
     'realmstore',
     'xbr',
